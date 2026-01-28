7e0b2180fe316d62e825df7bb14cc5b2eaf2450b
Wagh Ganesh <waghganesh580@gmail.com>
2026-01-28 16:07:29 +0530

    SCO

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -257,7 +257,8 @@ const BlogPage = () => {
         ))}
       </div> */}
       {/* Blog Cards */}
-      <div className="grid grid-cols-1 font-poppins md:grid-cols-2 lg:grid-cols-3 gap-10 items-start">
+      <div className="grid lg:px-10 xl:px-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+
         {blogs.map((blog, index) => (
           <motion.div
             key={blog.id}
